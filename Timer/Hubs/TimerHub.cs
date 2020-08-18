07af0b38fe1abe9259278ab0dfd2290322dcb7ee
Anas Al Kala <anas.kala@hotmail.com>
2020-08-18 08:45:53 +0200

    Add project files.

---
+++ Timer/Hubs/TimerHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer.Hubs
+{
+    public class TimerHub :Hub
+    {
+        public async Task SendMessage()
+        {
+            while (true)
+            {
+                var year = DateTime.Now.Year;
+                var month = DateTime.Now.Month;
+                var day = DateTime.Now.Day;                
+                var hour = DateTime.Now.Hour;
+                var minute = DateTime.Now.Minute;
+                var second = DateTime.Now.Second;
+                await Clients.All.SendAsync("ReceiveMessage", year,month,day,hour,minute,second);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
